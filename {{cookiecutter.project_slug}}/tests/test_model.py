57a2eefa05352731c9fe3bab3977c68e54fc3770
Marvin Gajek <Marvin.Gajek1@web.de>
2025-02-18 18:49:41 +0100

    refactor model build tests to use unittest.TestCase

--- {{cookiecutter.project_slug}}/tests/test_model.py
+++ {{cookiecutter.project_slug}}/tests/test_model.py
@@ -1,49 +1,56 @@
 {% if cookiecutter.ml_framework == 'tensorflow' %}
 
+import unittest
 from tensorflow.keras import layers, models
 from models.simple_nn import SimpleNN
 
+class TestBuildModel(unittest.TestCase):
+    def test_build(self):        
+        simple_nn = SimpleNN()
+        model = simple_nn.build()
 
-def test_build():
-    simple_nn = SimpleNN()
-    model = simple_nn.build()
-    assert isinstance(model, models.Sequential)
-    assert len(model.layers) == 3
-    assert isinstance(model.layers[0], layers.Flatten)
-    assert isinstance(model.layers[1], layers.Dense)
-    assert isinstance(model.layers[2], layers.Dense)
-    assert model.layers[1].units == 128
-    assert model.layers[2].units == 10
-    assert model.layers[2].activation.__name__ == "softmax"
+        self.assertIsInstance(model, models.Sequential)
+        
+        self.assertEqual(len(model.layers), 3)
+        
+        self.assertIsInstance(model.layers[0], layers.Flatten)
+        self.assertIsInstance(model.layers[1], layers.Dense)
+        self.assertIsInstance(model.layers[2], layers.Dense)
+        
+        self.assertEqual(model.layers[1].units, 128)
+        self.assertEqual(model.layers[2].units, 10)
+        
+        self.assertEqual(model.layers[2].activation.__name__, "softmax")
+
+if __name__ == "__main__":
+    unittest.main()
 
 {% elif cookiecutter.ml_framework == 'pytorch' %}
 
+import unittest
 import torch
 from models.simple_nn import SimpleNN
 
-def test_build():
-    simple_nn = SimpleNN()
-    model = simple_nn.build()
-    
-    assert isinstance(model, torch.nn.Module)
-    children = list(model.children())
-    
-    # Verify the number of layers
-    assert len(children) == 5  # Flatten, Linear, ReLU, Linear, Softmax
-    
-    # Verify types of layers
-    assert isinstance(children[0], torch.nn.Flatten)
-    assert isinstance(children[1], torch.nn.Linear)
-    assert isinstance(children[2], torch.nn.ReLU)
-    assert isinstance(children[3], torch.nn.Linear)
-    assert isinstance(children[4], torch.nn.Softmax)
-    
-    # Verify the properties of the linear layers
-    assert children[1].out_features == 128
-    assert children[3].out_features == 10
+class TestBuildModel(unittest.TestCase):
+    def test_build(self):
+        simple_nn = SimpleNN()
+        model = simple_nn.build()
+        
+        self.assertIsInstance(model, torch.nn.Module)
+
+        children = list(model.children())
+        self.assertEqual(len(children), 5)  
+        
+        self.assertIsInstance(children[0], torch.nn.Flatten)
+        self.assertIsInstance(children[1], torch.nn.Linear)
+        self.assertIsInstance(children[2], torch.nn.ReLU)
+        self.assertIsInstance(children[3], torch.nn.Linear)
+        self.assertIsInstance(children[4], torch.nn.Softmax)
+        
+        self.assertEqual(children[1].out_features, 128)
+        self.assertEqual(children[3].out_features, 10)
 
 if __name__ == "__main__":
-    test_build()
-    print("All tests passed!")
+    unittest.main()
 
 {% endif %}
